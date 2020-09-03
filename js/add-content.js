2ac47f97080ea267f73b819289f15aa619db19c9
malekshmes <malekshmes@gmail.com>
2020-09-03 22:08:40 +0300

    js codes added

--- js/add-content.js
+++ js/add-content.js
@@ -1,15 +1,54 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+// var today = new Date();
+// var hourNow = today.getHours();
 
-if (hourNow > 18) {
-    greeting = 'Good evening, Class!';
-  } else if (hourNow > 12) {
-    greeting = 'Good afternoon, Class!';
-  } else if (hourNow >= 0) {
-    greeting = 'Good morning!';
-  } else {
-    greeting = 'Something went wrong!';
+// function greeting(){
+//   // input
+//   var hourNow = prompt("what is the time?")
+//   var greeting;
+
+
+//   // proccessing
+//   if (hourNow > 18 && hourNow <=23) {
+//     greeting = "good evening class";
+//   } else if (hourNow > 12 && hourNow <= 18) {
+//     greeting = "good afternoon class";
+//   } else if (hourNow>= 0 && hourNow <= 12) {
+//     greeting = "good morning class";
+//   }else{
+//     greeting ="smth went wrong"
+//   }
+//   // output
+//   return greeting;
+// }
+
+// // console.log("new greeting "+ greeting);
+// document.write('<h3>' + greeting() + '</h3>');
+
+// function divide(x,y){
+//   console.log(x/y);
+// }
+
+// divide(10,10);
+// divide(9,3);
+var showOrder = function () {
+  var userInput = prompt("what would you choose a house or hotel");
+
+  while (userInput !== "hotel" && userInput !== "house"){
+     userInput = prompt("what would you choose a house or hotel");
   }
-document.write('<h3>'+greeting+'</h3>');
+  var userOrder= "";
+  var numberOfImage = prompt("how many images would you like to print? ")
+  for (var count = 0; count < numberOfImage; count++) {
+    if (userInput == "house") {
+      // document.write("<img src='images/house.png' alt=''");
+      userOrder = userOrder + "<img src='images/house.png' alt=''>"
+    }
+    else if (userInput == "hotel")
+      // document.write("<img src='images/hotel.png' alt=''");
+      userOrder = userOrder + "<img src='images/hotel.png' alt=''>"
 
+  }
+    return userOrder;
+
+  
+}
